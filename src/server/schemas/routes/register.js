0a6ae326d2787f9c535d18c71b32127df7d718fe
telemedbackend <telemedbackend@protonmail.com>
2018-11-20 18:05:16 +0300

    remove mobileNumber and expiry from schemas register and smsCheck

--- src/server/schemas/routes/register.js
+++ src/server/schemas/routes/register.js
@@ -3,7 +3,6 @@
 const Joi = require('joi');
 
 module.exports = Joi.object().keys({
-  mobileNumber: Joi.string().min(3).max(255), // TODO remove this field
   email: Joi.string().email({ minDomainAtoms: 2 }).required(),
   password: Joi.string().min(6).max(255).required(),
   surname: Joi.string().min(2).max(255).required(),
@@ -12,5 +11,4 @@ module.exports = Joi.object().keys({
   sex: Joi.any().valid(['Мужской', 'Женский']).required(),
   birthDate: Joi.date().required(),
   registerToken: Joi.string().min(16).required(),
-  expiry: Joi.date().timestamp('unix'), // TODO remove this field
 });
--- src/server/schemas/routes/smsCheck.js
+++ src/server/schemas/routes/smsCheck.js
@@ -4,8 +4,6 @@ const Joi = require('joi');
 
 module.exports = Joi.object().keys({
   smsToken: Joi.string().min(16).required(),
-  mobileNumber: Joi.string().min(3).max(255), // TODO remove this field
   smsCode: Joi.string().alphanum().lowercase().length(4)
     .required(),
-  expiry: Joi.date().timestamp('unix'), // TODO remove this field
 });
