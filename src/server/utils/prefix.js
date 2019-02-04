7d1da885c6d00553081a97c316e24bc397cc37db
telemedbackend <telemedbackend@protonmail.com>
2019-02-04 16:18:41 +0300

    pref

--- src/server/utils/prefix.js
+++ src/server/utils/prefix.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = (dbname) => {
-  if (!process.env.NODE_ENV) return `test_${dbname}`;
+  // if (!process.env.NODE_ENV) return `test_${dbname}`;
   if (process.env.NODE_ENV === 'production') return dbname;
   return `${process.env.NODE_ENV}_${dbname}`;
 };
