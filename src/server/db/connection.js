b1292bd7a783a5a0c2ba1031a1ec63390974e6d3
telemedbackend <telemedbackend@protonmail.com>
2019-02-04 16:36:07 +0300

    conn log

--- src/server/db/connection.js
+++ src/server/db/connection.js
@@ -4,4 +4,5 @@ const { couchdbUrlDev, couchdbUrlProd } = require('../../../secrets');
 
 const couchdbUrl = process.env.NODE_ENV === 'production' ? couchdbUrlProd : couchdbUrlDev;
 // process.env.COUCHDB_URL ||
+console.log(process.env.NODE_ENV, couchdbUrl);
 module.exports = require('nano')(couchdbUrl);
