fc3afd00f341c49190075d1be1a4085f36fb2d54
telemedbackend <telemedbackend@protonmail.com>
2019-03-12 15:54:16 +0300

    prettify handling JSON errors, hide error stack

--- src/server/middleware/catcher.js
+++ src/server/middleware/catcher.js
@@ -7,11 +7,23 @@ module.exports = () => async (ctx, next) => {
     await next();
   } catch (error) {
     log(error);
+    if (error.message.search(/JSON/) !== -1) {
+      ctx.status = 400;
+      ctx.body = {
+        status: 'error',
+        message: 'JSON format error',
+        error: {
+          message: error.message,
+          body: error.body,
+        },
+      };
+      return;
+    }
     ctx.status = 500;
     ctx.body = {
       status: 'error',
       message: 'Internal Server Error',
-      error,
+      error: error.message,
     };
   }
 };
