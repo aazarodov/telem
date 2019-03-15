33959caadeaec3c637036e2e2677c1a427132169
telemedbackend <telemedbackend@protonmail.com>
2019-03-15 16:27:09 +0300

    fix unread schema

--- src/server/schemas/routes/p/notifications/unread.js
+++ src/server/schemas/routes/p/notifications/unread.js
@@ -3,7 +3,8 @@
 const Joi = require('joi');
 
 module.exports = {
-  get: Joi.object().keys({}),
-  limit: Joi.number().positive().default(100),
-  bookmark: Joi.string().default(''),
+  get: Joi.object().keys({
+    limit: Joi.number().positive().default(100),
+    bookmark: Joi.string().default(''),
+  }),
 };
